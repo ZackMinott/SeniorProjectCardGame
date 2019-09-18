435b377506c28108a61441cae705a65c7c258c1a
Dominique Oyco <dominickoyco@gmail.com>
2019-09-18 12:30:20 -0700

    ...

--- Senior Project 491A/Assets/_Scripts/CardZoomer.cs
+++ Senior Project 491A/Assets/_Scripts/CardZoomer.cs
@@ -17,6 +17,13 @@ public class CardZoomer : MonoBehaviour
     {
         //Debug.Log("enter");
         transform.localScale += new Vector3(1.5F, 1.5F, 1.5F); //zooms in the object
+        //TODO: Move the cards up when it's on the bottom and move the cards down when it's on top when it's zoomed.
+    }
+
+
+    public void OnMouseDown()
+    {
+        //TODO: Minimize the card when its clicked
     }
 
     public void OnMouseExit()
